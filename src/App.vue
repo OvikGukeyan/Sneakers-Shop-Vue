ff143c8fa75f5d300413119604b20bcd41870a8b
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-12-16 13:07:34 +0100

    fetching

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,47 @@
 <script setup>
-import MainHeader from './components/Header.vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://40b6497860f9a336.mokky.dev/items')
+    items.value = data
+  } catch (error) {
+    console.error(error)
+  }
+})
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 mx-auto rounded-xl shadow-xl mt-14 pb-8">
-    <MainHeader />
-    <CardList />
+    <Header />
+    <div class="p-10">
+      <div class="flex justify-between items-center">
+        <h2 class="text-2xl font-bold mb-8">All Sneakers</h2>
+        <div class="flex gap-5">
+          <select class="py-2 px-3 border border-slate-200 rounded-md outline-none">
+            <option value="asc">Price: Low to High</option>
+            <option value="desc">Price: High to Low</option>
+            <option>By Name</option>
+          </select>
+          <div class="relative">
+            <img class="absolute top-3 left-3" src="/search.svg" alt="search" />
+            <input
+              placeholder="Search..."
+              class="border border-slate-200 py-2 pl-10 pr-4 rounded-md outline-none focus:border-slate-400"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
+    </div>
   </div>
 </template>
-
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,24 +1,25 @@
 <script setup>
+
 import Card from './Card.vue'
+
+
+defineProps({
+  items: Array,
+})
+
+
 </script>
 
 <template>
-  <div class="p-10">
-    <div class="flex justify-between">
-      <h2 class="text-2xl font-bold mb-8">All Sneakers</h2>
-        
-    </div>
-    <div class="grid grid-cols-4 gap-8">
-      <Card
-        image-url="/sneakers/sneakers-1.jpg"
-        title="Nike Air Max 270"
-        :price="25"
-        :is-added="true"
-        :is-bookmarked="true"
-      />
-      <Card />
-      <Card />
-      <Card />
-    </div>
+  <div class="grid grid-cols-4 gap-8">
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :image-url="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
+      :is-added="true"
+      :is-bookmarked="true"
+    />
   </div>
 </template>
