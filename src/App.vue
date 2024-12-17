3f9a09f4bf284ddc247787ffdcba78a14ee4c0e2
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-12-17 20:03:10 +0100

    filters

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
@@ -7,14 +7,42 @@ import Drawer from './components/Drawer.vue'
 
 const items = ref([])
 
-onMounted(async () => {
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearch = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
-    const { data } = await axios.get('https://40b6497860f9a336.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy,
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get('https://40b6497860f9a336.mokky.dev/items',
+      {
+        params: params
+      }
+    )
     items.value = data
   } catch (error) {
     console.error(error)
   }
-})
+}
+
+onMounted(fetchItems)
+
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -25,14 +53,19 @@ onMounted(async () => {
       <div class="flex justify-between items-center">
         <h2 class="text-2xl font-bold mb-8">All Sneakers</h2>
         <div class="flex gap-5">
-          <select class="py-2 px-3 border border-slate-200 rounded-md outline-none">
-            <option value="asc">Price: Low to High</option>
-            <option value="desc">Price: High to Low</option>
-            <option>By Name</option>
+          <select
+            @change="onChangeSelect"
+            class="py-2 px-3 border border-slate-200 rounded-md outline-none"
+          >
+            <option value="price">Price: Low to High</option>
+            <option value="-price">Price: High to Low</option>
+            <option value="title">By Name</option>
           </select>
           <div class="relative">
             <img class="absolute top-3 left-3" src="/search.svg" alt="search" />
             <input
+              @input="onChangeSearch"
+              type="text"
               placeholder="Search..."
               class="border border-slate-200 py-2 pl-10 pr-4 rounded-md outline-none focus:border-slate-400"
             />
